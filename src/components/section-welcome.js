1d207356c24c2b2a13627a07b1ada74d0597afd1
hoodini47 <msajdera@gmail.com>
2020-11-04 18:37:35 +0100

    Section welcome photo fix.

--- src/components/section-welcome.js
+++ src/components/section-welcome.js
@@ -3,38 +3,39 @@ import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import "../styles/section-welcome.scss"
 import ArrowLogo from "../images/arrow.inline.svg"
-import scrollTo from 'gatsby-plugin-smoothscroll'
+import scrollTo from "gatsby-plugin-smoothscroll"
 
 export const SectionWelcome = () => {
-
-  const data =  useStaticQuery(graphql`
-  query MyMainPhotoQuery {
-    datoCmsMainPhoto {
-      mainPhoto {
-        fluid {
-          src
-          srcSet
-          height
-          width
-          base64
+  const data = useStaticQuery(graphql`
+    query MyMainPhotoQuery {
+      datoCmsMainPhoto {
+        mainPhoto {
+          fluid {
+            src
+            srcSet
+            height
+            width
+          }
         }
       }
     }
-  }
-  
-`)
+  `)
 
- return (
-  <section id={`welcome`} className={`first-section`}>
-    <Img fadeIn fluid={data.datoCmsMainPhoto.mainPhoto.fluid}/>
-    <div role="button" tabIndex={0} className={`scroll-icon`} onClick={() => scrollTo('.about-content-container')}>
-      <span className={`arrow-container`}><ArrowLogo /></span>
-    </div>
-  </section>
- )
+  return (
+    <section id={`welcome`} className={`first-section`}>
+      <Img fadeIn fluid={data.datoCmsMainPhoto.mainPhoto.fluid} />
+      <div
+        role="button"
+        tabIndex={0}
+        className={`scroll-icon`}
+        onClick={() => scrollTo(".about-content-container")}
+      >
+        <span className={`arrow-container`}>
+          <ArrowLogo />
+        </span>
+      </div>
+    </section>
+  )
 }
 
 export default SectionWelcome
-
-
-
